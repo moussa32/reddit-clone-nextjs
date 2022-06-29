9ccf0c91429f5e16eccd926325446b92424fb01e
mozes32 <moussaebrahem32@gmail.com>
2022-06-29 10:39:58 +0200

    fix: navbar icon size

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { Flex, Image as ChakraImg } from "@chakra-ui/react";
-import Image from "next/image";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
@@ -10,14 +10,13 @@ const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
-        <Image src="/images/redditFace.svg" width={40} height={30} />
-        <ChakraImg src="/images/redditText.svg" height={46} display={{ base: "none", md: "unset" }} />
+      <Flex align="center" width={{ base: "40px", md: "auto" }} mr={{ base: 0, md: 2 }} cursor="pointer">
+        <ChakraImg src="/images/redditFace.svg" height="30px" />
+        <ChakraImg display={{ base: "none", md: "unset" }} src="/images/redditText.svg" height="46px" />
       </Flex>
+      <Directory />
       <SearchInput />
       <RightContent user={user} />
-      {/* <Directory />
-       */}
     </Flex>
   );
 };
