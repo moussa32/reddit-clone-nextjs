962f3f3b4e40844072be7cb609550fcba4b7bcfb
mozes32 <moussaebrahem32@gmail.com>
2022-06-29 10:20:01 +0200

    refactor: changing user menu

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,17 +1,35 @@
+import React from "react";
 import { FaRedditSquare } from "react-icons/fa";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { VscAccount } from "react-icons/vsc";
-import { IoSparkles } from "react-icons/io5";
+import { IoSparkles, IoLogInOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
-import { Flex, Icon, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { User } from "firebase/auth";
-import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import {
+  Avatar,
+  AvatarBadge,
+  Box,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { signOut, User } from "firebase/auth";
+import { auth } from "../../../firebase/clientApp";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <Menu>
       <MenuButton
@@ -20,25 +38,80 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
         borderRadius={4}
         _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
       >
-        {user ? (
-          <Flex align="center">
-            <Flex align="center">
+        <Flex align="center">
+          <Flex align="center" minWidth={{ base: "auto", md: 180 }}>
+            {user ? (
               <>
-                <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
+                <Avatar
+                  height="100%"
+                  width={30}
+                  color="gray.300"
+                  bg="transparent"
+                  mr={1}
+                  icon={<FaRedditSquare fontSize="1.8rem" />}
+                >
+                  <AvatarBadge boxSize="0.7em" bg="green.400" />
+                </Avatar>
+                <Flex
+                  direction="column"
+                  display={{ base: "none", md: "flex" }}
+                  fontSize="8pt"
+                  align="flex-start"
+                  mr={8}
+                >
+                  <Text fontWeight={700}>{user?.displayName || user.email?.split("@")[0]}</Text>
+                  <Flex align="center">
+                    <Icon color="brand.100" mr={1} as={IoSparkles} />
+                    <Text color="gray.500">1 karam</Text>
+                  </Flex>
+                </Flex>
               </>
-              <ChevronDownIcon />
-            </Flex>
+            ) : (
+              <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
+            )}
+            <Box ml="auto" color="gray.400">
+              <IoIosArrowDown size={"1.2rem"} />
+            </Box>
           </Flex>
-        ) : (
-          <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
-        )}
+        </Flex>
       </MenuButton>
       <MenuList>
-        <MenuItem>
-          <Flex>
-            <Icon as={CgProfile} />
-          </Flex>
-        </MenuItem>
+        {user ? (
+          <>
+            <MenuItem fontSize="10pt" fontWeight={700} _hover={{ bg: "blue.500", color: "white" }}>
+              <Flex align="center">
+                <Icon as={CgProfile} fontSize={20} mr={2} />
+                Profile
+              </Flex>
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem
+              fontSize="10pt"
+              fontWeight={700}
+              _hover={{ bg: "blue.500", color: "white" }}
+              onClick={() => signOut(auth)}
+            >
+              <Flex align="center">
+                <Icon as={IoLogInOutline} fontSize={20} mr={2} />
+                Log Out
+              </Flex>
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem
+              fontSize="10pt"
+              fontWeight={700}
+              _hover={{ bg: "blue.500", color: "white" }}
+              onClick={() => setAuthModalState({ open: true, view: "login" })}
+            >
+              <Flex align="center">
+                <Icon as={IoLogInOutline} fontSize={20} mr={2} />
+                Log In / Sign Up
+              </Flex>
+            </MenuItem>
+          </>
+        )}
       </MenuList>
     </Menu>
   );
